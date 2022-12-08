463b590379e9116ad3738e31e130ff6bb6e10ee6
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-07 21:49:05 -0500

    template literal added, subclasses created for manager, and role files linked

--- index.js
+++ index.js
@@ -2,41 +2,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/employee');
-const ranchHand = require('./lib/ranchHand')
+// const ranchHand = require('./lib/ranchHand')
 
 // function that has html template
 //      includes ${create employee card} tag within
-const generateHTML = ({}) =>
-`<!DOCTYPE html>....`
+// const generateHTML = ({}) =>
+// `<!DOCTYPE html>....`
 
-const renderEmployeeCard = ({fullName, role, email, stall, horseName, gitHub}) =>
-`<div class="card" style="width: 18rem;">
-<div class="card-header">
-    ${fullName}
-</div>
-<ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-</ul>
-</div>`
+// const renderEmployeeCard = ({fullName, role, email, stall, horseName, gitHub}) =>
+// `<div class="card" style="width: 18rem;">
+// <div class="card-header">
+//     ${fullName}
+// </div>
+// <ul class="list-group list-group-flush">
+//     <li class="list-group-item">Cras justo odio</li>
+//     <li class="list-group-item">Dapibus ac facilisis in</li>
+//     <li class="list-group-item">Vestibulum at eros</li>
+// </ul>
+// </div>`
 // func to generateCowboyCard
 
 
 // inquirer .prompt with card input questions
 // questions for Manager first/mandatory (then other employees)
 inquirer
-  .prompt(managerQs)
-  .then(managerAnswers)
-  
-  .prompt(cowboyQs)
-  .then(cowboyAnswers)
-
-  const managerQs = [
+  .prompt([
     {
-      name: 'fullName',
       type: 'input',
-      message: 'Employee full name'
+      message: 'Employee full name',
+      name: 'fullName',
     },
     {
         name: 'role',
@@ -58,7 +52,39 @@ inquirer
         type: 'input',
         message: 'Stall number'
       }
-  ]
+  ])
+//  ?? Can i eventually do .prompt(managerAnswers, cowboyAnswers)
+  
+//   .prompt(cowboyQs)
+//   .then(cowboyAnswers)
+
+//   const managerQs = [
+//     {
+//       name: 'fullName',
+//       type: 'input',
+//       message: 'Employee full name'
+//     },
+//     {
+//         name: 'role',
+//         type: 'choice',
+//         message: ['Ranch Manager']
+//       },
+//       {
+//         name: 'email',
+//         type: 'input',
+//         message: 'Employee email'
+//       },
+//       {
+//         name: 'horseName',
+//         type: 'input',
+//         message: 'Horse name'
+//       },
+//       {
+//         name: 'stall',
+//         type: 'input',
+//         message: 'Stall number'
+//       }
+//   ]
 
   const cowboyQs = [
   {
@@ -95,9 +121,12 @@ inquirer
 
 // .then writeFile method template filled with answers 
 .then((managerAnswers) => {
-    const htmlPageContent = generateHTML(managerAnswers);
+    const htmlTemplate = new Employee(managerAnswers);
+    // htmlTemplate.generateHtml()
+    // need to make new instance? to then call the generateHtml() on that instance?
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
+    fs.writeFile('index.html', htmlTemplate.generateHtml(), (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
+    // second fs.writeFile('style.css') to import my styles??
   });
